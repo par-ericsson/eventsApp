d9e2fc01594e851f453881e3935d3602c043fae9
Pär Ericsson <ericsson@home.se>
2020-05-24 14:07:13 +0200

    Added more functionality to form

--- src/features/events/EventDashboard/EventDashboard.jsx
+++ src/features/events/EventDashboard/EventDashboard.jsx
@@ -8,7 +8,7 @@ const eventsFromDashboard = [
   {
     id: '1',
     title: 'Trip to Tower of London',
-    date: '2020-05-10T11:00:00+00:00',
+    date: '2020-05-10',
     category: 'culture',
     description:
       'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Phasellus sollicitudin ligula eu leo tincidunt, quis scelerisque magna dapibus. Sed eget ipsum vel arcu vehicula ullamcorper.',
@@ -32,14 +32,14 @@ const eventsFromDashboard = [
   {
     id: '2',
     title: 'Trip to Punch and Judy Pub',
-    date: '2020-05-11T14:00:00+00:00',
+    date: '2020-05-11',
     category: 'drinks',
     description:
       'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Phasellus sollicitudin ligula eu leo tincidunt, quis scelerisque magna dapibus. Sed eget ipsum vel arcu vehicula ullamcorper.',
     city: 'London, UK',
     venue: 'Punch & Judy, Henrietta Street, London, UK',
     hostedBy: 'Lynda',
-    hostPhotoURL: 'https://randomuser.me/api/portraits/women/22.jpg',
+    hostPhotoURL: 'https://randomuser.me/api/portraits/women/21.jpg',
     attendees: [
       {
         id: 'b',
@@ -59,14 +59,28 @@ const eventsFromDashboard = [
 class EventDashboard extends Component {
   state = {
     events: eventsFromDashboard,
-    isOpen: false
-  }
+    isOpen: false,
+    selectedEvent: null
+  };
 
-  handleIsOpenToggle = () => {
+ /*  handleIsOpenToggle = () => {
     this.setState((prevState) => ({
       isOpen: !prevState.isOpen
     }))
-  }
+  }; */
+
+  handleCreateFormOpen = () => {
+    this.setState({
+      isOpen: true,
+      selectedEvent: null
+    })
+  };
+
+  handleFormCancel = () => {
+    this.setState({
+      isOpen: false
+    })
+  };
 
   handleCreateEvent = (newEvent) => {
     newEvent.id = cuid();
@@ -74,19 +88,58 @@ class EventDashboard extends Component {
     this.setState(({events}) => ({
       events: [...events, newEvent],
       isOpen: false
+    }));
+  };
+
+  handleSelectEvent = (event) => {
+    this.setState({
+      selectedEvent: event,
+      isOpen: true
+    })
+  };
+
+  handleUpdateEvent = (updatedEvent) => {
+    this.setState(({events}) => ({
+      events: events.map(event => {
+        if(event.id === updatedEvent.id) {
+          return {...updatedEvent}
+        } else {
+          return event
+        }
+      }),
+      isOpen: false,
+      selectedEvent: null
     }))
-  }
+  };
+
+  handleDeleteEvent = (id) => {
+    this.setState(({events}) => ({
+      events: events.filter(e => e.id !== id) 
+    }))
+  };
 
     render() {
-      const {events, isOpen} = this.state;
+      const {events, isOpen, selectedEvent} = this.state;
       return (
         <Grid>
           <Grid.Column width={10}>
-            <EventList events={events}/>
+            <EventList 
+              events={events} 
+              selectEvent={this.handleSelectEvent}
+              deleteEvent={this.handleDeleteEvent}
+            />
           </Grid.Column>
           <Grid.Column width={6}>
-            <Button onClick={this.handleIsOpenToggle} positive content='Create Event'/>
-            {isOpen && <EventForm createEvent={this.handleCreateEvent} cancelFormOpen={this.handleIsOpenToggle} />}
+            <Button onClick={this.handleCreateFormOpen} positive content='Create Event'/>
+            {isOpen && (
+              <EventForm 
+                key={selectedEvent ? selectedEvent.id : 0}
+                updateEvent={this.handleUpdateEvent} 
+                selectedEvent={selectedEvent} 
+                createEvent={this.handleCreateEvent} 
+                cancelFormOpen={this.handleFormCancel} 
+              />
+            )}
           </Grid.Column>
         </Grid>
       )
